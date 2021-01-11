6129631c9817a7d02f5581a15f8dddb97aeb66ab
Tom Goldsmith <tom@7theta.com>
2021-01-11 11:59:25 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  [org.clojure/clojurescript "1.10.773"]
                  [com.7theta/utilis "1.10.0"]
                  [metosin/malli "0.2.1"]
-                 [borkdude/sci "0.1.1-alpha.10"]
-                 [reagent "1.0.0-rc1"]]
+                 [borkdude/sci "0.2.1-alpha.1"]
+                 [reagent "1.0.0"]]
   :profiles {:dev {:source-paths ["src" "example/src"]
-                   :dependencies [[thheller/shadow-cljs "2.11.8"]]}})
+                   :dependencies [[thheller/shadow-cljs "2.11.13"]]}})
