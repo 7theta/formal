91fb0ac5257a17f83aee75591bd00253d67a6e72
Tom Goldsmith <tom@7theta.com>
2021-02-18 15:50:57 -0500

    Default values are reported on component did mount.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.7theta/formal "0.1.0"
+(defproject com.7theta/formal "0.1.1"
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/clojurescript "1.10.773"]
                  [com.7theta/utilis "1.12.1"]
